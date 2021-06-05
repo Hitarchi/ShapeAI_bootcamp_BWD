fc074847625a08b21ea3600a6c25706a4a8c7ded
Hitarchi <hitarchibhardwaj@gmail.com>
2021-06-05 23:32:41 +0530

    project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Review on ShapeAI's bootcamp</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.Js</h1>
+      <p>
+        This was an amazing bootcamp in which I have learnt a lot about
+        JavaScript and React.Js. Everything was explained so well. Will love to
+        join more such bootcamps from ShapeAI. This bootcamp was taken up by
+        Shaurya Sinha and such a great teacher he is. He explained complex to
+        complex topics so easily.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
